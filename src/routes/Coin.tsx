0809744a380f7a562c447b2e26a79a660c21c923
TaekyungAn <atk0426@naver.com>
2023-07-07 23:12:50 +0900

    #5.6 Data Types

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -30,12 +30,72 @@ interface RouteState {
   state: { name: string };
 }
 
+// shift + alt + i (다중선택 후 커서 맨 뒤로 보내기)
+interface IInfoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  logo: string;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  first_data_at: string;
+  last_data_at: string;
+}
+
+interface IPriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      ath_date: string;
+      ath_price: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+    };
+  };
+}
+
 function Coin() {
   const [loading, setLoading] = useState(true);
+  const [info, setInfo] = useState<IInfoData>();
+  const [priceInfo, setPriceInfo] = useState<IPriceData>();
   // useLocation : Link에서 주는 정보 받아옴
   // 부모 컴포넌트에서 fetch된 data(state)를 Link를 통해 받아올 수 있다.
   const { state } = useLocation() as RouteState;
   const { coinId } = useParams<"coinId">();
+
   useEffect(() => {
     (async () => {
       const infoData = await axios(
