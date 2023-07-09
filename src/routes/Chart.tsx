241aa4ab7cc8fa703a06d7dfe5361c339bfbd29e
TaekyungAn <atk0426@naver.com>
2023-07-09 15:18:01 +0900

    #5.13 Price Chart part Two

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,17 +1,72 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchCoinHistory } from "../api";
 import { useOutletContext } from "react-router-dom";
-interface ChartProps {
+import ReactApexChart from "react-apexcharts";
+interface IChartProps {
   coinId: string;
+}
+interface IHistorical {
+  time_open: number;
+  time_close: number;
+  open: string;
+  high: string;
+  low: string;
+  close: string;
+  volume: string;
+  market_cap: number;
 }
 function Chart() {
   // 부모 Outlet context로 넘겨받은 props => useOutletContext로 받아오기
-  const { coinId } = useOutletContext<ChartProps>();
+  const { coinId } = useOutletContext<IChartProps>();
   console.log(coinId);
 
-  const { isLoading, data } = useQuery(["ohlcv", coinId], () =>
+  const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
     fetchCoinHistory(coinId)
   );
-  return <h1>Chart</h1>;
+  return (
+    <div>
+      {isLoading ? (
+        "Loading chart..."
+      ) : (
+        <ReactApexChart
+          type="line"
+          series={[
+            {
+              name: "Price",
+              // 니꼬샘 API는 close 데이터가 string이기 때문에 parseFloat를 통해 형 변환
+              // ?? [] : 데이터가 null이 되는 걸 방지, null 대신 빈 배열로 바꿔줌
+              data: data?.map((price) => parseFloat(price.close)) ?? [],
+            },
+          ]}
+          options={{
+            theme: {
+              mode: "dark",
+            },
+            chart: {
+              height: 300,
+              width: 500,
+              toolbar: {
+                show: false,
+              },
+              background: "transparent",
+            },
+            grid: { show: false },
+            stroke: {
+              curve: "smooth",
+              width: 4,
+            },
+            yaxis: {
+              show: false,
+            },
+            xaxis: {
+              axisBorder: { show: false },
+              axisTicks: { show: false },
+              labels: { show: false },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 }
 export default Chart;
