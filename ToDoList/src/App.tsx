b3fbe96e0ac602f041697bc38762de183dd42686
TaekyungAn <atk0426@naver.com>
2023-07-11 17:11:20 +0900

    #6.5 To Do Setup

--- ToDoList/src/App.tsx
+++ ToDoList/src/App.tsx
@@ -1,6 +1,19 @@
 import React from "react";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+import ToDoList from "./ToDoList";
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  /* other styles */
+`;
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <GlobalStyle />
+      <ToDoList />
+    </>
+  );
 }
 
 export default App;
---
+++ ToDoList/src/styled.d.ts
@@ -0,0 +1,11 @@
+import "styled-components";
+
+// styled-components 테마의 정의 확장
+declare module "styled-components" {
+  export interface DefaultTheme {
+    textColor: string;
+    bgColor: string;
+    accentColor: string;
+    cardBgColor: string;
+  }
+}
