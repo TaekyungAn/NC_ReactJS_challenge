131b329f6f7f46dd7739ac6275efc99d0e6f72ba
TaekyungAn <atk0426@naver.com>
2023-07-12 22:24:35 +0900

    #6.16 Selectors part One

--- ToDoList/src/atoms.tsx
+++ ToDoList/src/atoms.tsx
@@ -1,4 +1,4 @@
-import { atom } from "recoil";
+import { selector, atom } from "recoil";
 
 export interface IToDo {
   text: string;
@@ -8,4 +8,17 @@ export interface IToDo {
 export const toDoState = atom<IToDo[]>({
   key: "toDo",
   default: [],
+});
+
+// selector: atom의 output을 변형한 값만 담아올 수 있음
+export const toDoSelector = selector({
+  key: "toDoSelector",
+  get: ({ get }) => {
+    const toDos = get(toDoState);
+    return [
+      toDos.filter((toDo) => toDo.category === "TO_DO"),
+      toDos.filter((toDo) => toDo.category === "DOING"),
+      toDos.filter((toDo) => toDo.category === "DONE"),
+    ];
+  },
 });
--- ToDoList/src/components/ToDoList.tsx
+++ ToDoList/src/components/ToDoList.tsx
@@ -1,20 +1,36 @@
 import { useRecoilValue } from "recoil";
 import CreateToDo from "./CreateToDo";
-import { toDoState } from "../atoms";
+import { toDoSelector } from "../atoms";
 import ToDo from "./ToDo";
 
 function ToDoList() {
-  const toDos = useRecoilValue(toDoState);
-  console.log(toDos);
+  const [toDo, doing, done] = useRecoilValue(toDoSelector);
+
   return (
     <div>
       <h1>ToDos</h1>
       <CreateToDo />
+      <h2>To Do</h2>
       <ul>
-        {toDos.map((toDo) => (
+        {toDo.map((toDo) => (
           <ToDo key={toDo.id} {...toDo} />
         ))}
       </ul>
+      <hr />
+      <h2>Doing</h2>
+      <ul>
+        {doing.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+      <hr />
+      <h2>Done</h2>
+      <ul>
+        {done.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+      <hr />
     </div>
   );
 }
